0fcf946f2e15d3eca882d97484cdb8e71650cbcb
Francisco de la Vega <fdelavega@conwet.com>
2018-03-01 16:53:16 +0100

    Include a docker image for development

---
+++ docker/deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Compile sources
+cd /party
+mvn install
+
+# Create database if needed
+MYSQL_HOST=mysql.docker
+MYSQL_ROOT_PASSWORD=toor
+MYSQL_URL="jdbc\:mysql\://${MYSQL_HOST}\:3306/DSPARTYMANAGEMENT"
+
+mysql -u root --password=${MYSQL_ROOT_PASSWORD} -h ${MYSQL_HOST} -e "CREATE DATABASE IF NOT EXISTS DSPARTYMANAGEMENT;"
+
+# Create database jdbc conection and deploy the war
+
+export PATH=$PATH:/glassfish4/glassfish/bin
+asadmin start-domain
+
+asadmin create-jdbc-connection-pool --restype java.sql.Driver --driverclassname com.mysql.jdbc.Driver --property user=root:password=${MYSQL_ROOT_PASSWORD}:URL=${MYSQL_URL} DSPARTYMANAGEMENT
+
+asadmin create-jdbc-resource --connectionpoolid DSPARTYMANAGEMENT jdbc/partydb
+
+asadmin deploy --force true --contextroot DSPartyManagement --name DSPartyManagement /party/target/DSPartyManagement.war
